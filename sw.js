548e556af97d3664c7f7948924301b6a49ba4000
ICOACH <yunagunwoo@gmail.com>
2026-04-01 22:25:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'bookquest-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/favicon.png',
+  './icons/app-icon-192.png',
+  './icons/app-icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
